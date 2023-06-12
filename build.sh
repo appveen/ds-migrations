b261422318071583bf1eb4784516aa3a665fee1a
Jugnu Agrawal <jugnu@appveen.com>
2023-06-12 13:20:38 +0530

    build fix

--- build.sh
+++ build.sh
@@ -5,3 +5,8 @@ fi
 echo "Building Docker Image using tag: $1"
 
 docker build -t odp-datastack-migration:$1 .
+
+if [ $pushToDockerHub ]; then
+    docker tag odp-datastack-migration:$1 appveen/odp-datastack-migration:$1
+    docker push appveen/odp-datastack-migration:$1
+fi
