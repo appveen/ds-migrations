edd70ab6f0ca678d69c5c45121ebc7986a57724e
Jugnu Agrawal <jugnu@appveen.com>
2023-12-08 22:20:18 +0530

    running code in local

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 if (process.env.NODE_ENV != 'production') {
     require('dotenv').config();
 }
-const fs = require('fs');
+// const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const log4js = require('log4js');
 
-const migrationUtils = require('./migration.utils');
+// const migrationUtils = require('./migration.utils');
 
 const PORT = process.env.PORT || 3000;
 const LOG_LEVEL = process.env.LOG_LEVEL || 'info';
@@ -19,28 +19,28 @@ log4js.configure({
     categories: { default: { appenders: ['out'], level: LOG_LEVEL } }
 });
 
-try {
-    if (process.env.NODE_ENV == 'production') {
-        global.baseKey = fs.readFileSync('/opt/keys/odp.key', 'utf8');
-        global.baseCert = fs.readFileSync('/opt/keys/odp.crt', 'utf8');
-        global.ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
+// try {
+//     if (process.env.NODE_ENV == 'production') {
+//         global.baseKey = fs.readFileSync('/opt/keys/odp.key', 'utf8');
+//         global.baseCert = fs.readFileSync('/opt/keys/odp.crt', 'utf8');
+//         global.ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
 
-        if (!global.ENCRYPTION_KEY) {
-            throw new Error('Please provide ENCRYPTION_KEY as ENV Variable');
-        }
-        if (!global.baseKey || !global.baseCert) {
-            throw new Error('Please mount odp-sec secret in the deployment yaml');
-        }
-    } else {
-        global.baseKey = fs.readFileSync('./odp.key', 'utf8');
-        global.baseCert = fs.readFileSync('./odp.crt', 'utf8');
-        global.ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
-    }
-    migrationUtils.checkKeyAndCert(global.baseKey, global.baseCert);
-} catch (err) {
-    logger.error(err);
-    process.exit(0);
-}
+//         if (!global.ENCRYPTION_KEY) {
+//             throw new Error('Please provide ENCRYPTION_KEY as ENV Variable');
+//         }
+//         if (!global.baseKey || !global.baseCert) {
+//             throw new Error('Please mount odp-sec secret in the deployment yaml');
+//         }
+//     } else {
+//         global.baseKey = fs.readFileSync('./odp.key', 'utf8');
+//         global.baseCert = fs.readFileSync('./odp.crt', 'utf8');
+//         global.ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
+//     }
+//     migrationUtils.checkKeyAndCert(global.baseKey, global.baseCert);
+// } catch (err) {
+//     logger.error(err);
+//     process.exit(0);
+// }
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json({ inflate: true }));
